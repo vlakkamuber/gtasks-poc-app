76d883a169c0840f18b8fe01aed6c0e2daf77a3d
sameer mishra <smishr81@ext.uber.com>
2023-10-28 06:22:27 +0530

    enter otp login screen added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
 
-const Login: React.FC = () => {
+import React, { useState } from 'react';
+import { IonInput, IonLabel, IonItem,IonButton,IonContent, IonPage, } from '@ionic/react';
+import { star } from 'ionicons/icons';
+// import {Selectable} from 'ionic-selectable';
+
+// Define a function to fetch the list of countries
+function fetchCountries() {
+  // You can fetch countries data from an API or use a predefined list
+  // For this example, let's use a predefined list
+  return [
+    { id: 'US', name: 'United States', dialCode: '+1' },
+    { id: 'CA', name: 'Canada', dialCode: '+1' },
+    // Add more countries as needed
+  ];
+}
+
+const Login = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  // Handle the selection of a country
+  // const handleCountryChange = (event) => {
+  //   setSelectedCountry(event);
+  // };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      {/* Add your login form and UI components here */}
-    </div>
+    <IonPage>
+    <IonContent>
+    <IonItem>
+      <IonLabel position="stacked"><h1>What's your number?</h1></IonLabel>
+      <IonInput type="tel" placeholder="Enter your phone number" />
+      <div style={{ display: 'flex', justifyContent: 'space-between', width:'100%' }}>
+      <IonButton routerLink="/otp" fill="clear">Previous</IonButton>
+        <IonButton routerLink="/otp" fill="clear">Next</IonButton>
+      </div>
+     
+    </IonItem>
+    </IonContent>
+    </IonPage>
   );
 };
 
--- src/pages/OTP.tsx
+++ src/pages/OTP.tsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React, { useState, useRef } from "react";
+import {
+  IonInput,
+  IonLabel,
+  IonItem,
+  IonButton,
+  IonContent,
+  IonPage,
+} from "@ionic/react";
+import "./Tab2.css";
 
 const OTP: React.FC = () => {
+  const [otp, setOtp] = useState(["", "", "", "", "", ""]);
+  const inputRefs = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+  ];
+
+  const handleInputChange = (e, index) => {
+    if (e.target.value.length === 1 && index < 5) {
+      inputRefs[index + 1].current.focus();
+    }
+
+    const updatedOtp = [...otp];
+    updatedOtp[index] = e.target.value;
+    setOtp(updatedOtp);
+  };
   return (
-    <div>
-      <h1>OTP Page</h1>
-      {/* Add your login form and UI components here */}
-    </div>
+    <IonPage>
+      <IonContent>
+        <h2>Enter the OTP:</h2>
+        <div className="otp-input">
+          {otp.map((value, index) => (
+            <input
+              key={index}
+              ref={inputRefs[index]}
+              type="text"
+              value={value}
+              onChange={(e) => handleInputChange(e, index)}
+              maxLength={1}
+            />
+          ))}
+        </div>
+        {/* Add a button to submit the OTP and handle the verification logic */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <IonButton routerLink="/login" fill="clear">
+            Previous
+          </IonButton>
+          <IonButton routerLink="/dashboard" fill="clear">
+            Next
+          </IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
