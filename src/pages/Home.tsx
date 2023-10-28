3e79abcb7eb1b76d95283aeacad74e016c098cb7
sameer mishra <smishr81@ext.uber.com>
2023-10-28 06:58:25 +0530

    dashboard home page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonCardContent,IonButtons,IonButton} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import { useHistory } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const history = useHistory();
+
+  const goBack = () => {
+    history.goBack(); // This function navigates back to the previous page
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
+        <IonButtons slot="start">
+            <IonButton onClick={goBack}>Back</IonButton>
+          </IonButtons>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
@@ -16,7 +25,33 @@ const Home: React.FC = () => {
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Home" />
+        <IonCard>
+      <img alt="Silhouette of mountains" src="public/assets/text_to_audio.png" />
+      <IonCardHeader>
+        <IonCardTitle>Text to Audio • Localise,Record</IonCardTitle>
+        {/* <IonCardSubtitle>Card Subtitle</IonCardSubtitle> */}
+      </IonCardHeader>
+
+      <IonCardContent>Read the documents which is assigned with local language or in English and record the voice with the same .</IonCardContent>
+    </IonCard>
+    <IonCard>
+      <img alt="Silhouette of mountains" src="public/assets/audio_to_audio.png" />
+      <IonCardHeader>
+        <IonCardTitle>Audio to Text • Localise,Record</IonCardTitle>
+        {/* <IonCardSubtitle>Card Subtitle</IonCardSubtitle> */}
+      </IonCardHeader>
+
+      <IonCardContent>Read the documents which is assigned with local language or in English and record the voice with the same .</IonCardContent>
+    </IonCard>
+    <IonCard>
+      <img alt="Silhouette of mountains" src="public/assets/text_to_image.png" />
+      <IonCardHeader>
+        <IonCardTitle>Text to Image • Localise,Record</IonCardTitle>
+        {/* <IonCardSubtitle>Card Subtitle</IonCardSubtitle> */}
+      </IonCardHeader>
+
+      <IonCardContent>Read the documents which is assigned with local language or in English and record the voice with the same .</IonCardContent>
+    </IonCard>
       </IonContent>
     </IonPage>
   );
