88b9c373760f7cde2f1ca1a81521f215638e2739
sameer mishra <smishr81@ext.uber.com>
2023-11-08 10:08:59 +0530

    button position changes

--- src/pages/LoginSuccess.tsx
+++ src/pages/LoginSuccess.tsx
@@ -4,7 +4,8 @@ import {
     IonButton,
   } from "@ionic/react";
   import { useHistory } from 'react-router-dom';
-  
+  import {Block} from 'baseui/block';
+import {Button, KIND} from 'baseui/button';
   const LoginSuccess: React.FC = () => {
       const history = useHistory()
       const goToHome = ()=>{
@@ -19,8 +20,9 @@ import {
               <h2 style={{margin:'0'}}>Thanks,</h2>
               <h2 style={{margin:'0'}}>ABC!</h2>
               <p style={{margin:'0'}}>Your account has been created</p>
-              <div className="button-container-completed">
-                <IonButton expand="full" color="primary" style={{width:'100%',height:'40px'}} onClick={()=>goToHome()}>Continue</IonButton>
+              <div className="button-login-success" style={{marginTop: '90px'}}>
+                {/* <IonButton expand="full" color="primary" style={{width:'100%',height:'40px'}} onClick={()=>goToHome()}>Continue</IonButton> */}
+                <Button kind={KIND.primary} onClick={()=>goToHome()}>Continue</Button>
               </div>
             </div>
           </IonContent>
