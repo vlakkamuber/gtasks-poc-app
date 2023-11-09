7c4518ffe171f291d4ddd50ad6ecbde467c6cbe4
sameer mishra <smishr81@ext.uber.com>
2023-11-09 13:06:27 +0530

    latest build added

--- dist/assets/swipe-back-legacy-a10d8c2e.js
+++ dist/assets/swipe-back-legacy-a10d8c2e.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5d1bfab4.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-acf54481.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
