76e0943de19fd0093048933a2cffa9e6f3652124
Vinay Lakkam <vlakka1@ext.uber.com>
2024-01-13 14:02:46 +0530

    added options to the gateway

--- dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/DcagApiServiceJavaApplication.java
+++ dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/DcagApiServiceJavaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +32,7 @@ public class DcagApiServiceJavaApplication {
 }
 
 @RestController
+@CrossOrigin
 class RootController {
     @RequestMapping("/")
     String root() {
