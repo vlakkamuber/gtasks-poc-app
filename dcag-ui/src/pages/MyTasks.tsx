9602fa2f976a3a7ccb6a82b99debaeadb3a8163f
sameer mishra <smishr81@ext.uber.com>
2024-01-15 23:49:10 +0530

    hardcoded userid removed

--- dcag-ui/src/pages/MyTasks.tsx
+++ dcag-ui/src/pages/MyTasks.tsx
@@ -1,18 +1,15 @@
-import React, { useState,useEffect } from "react";
-import {
-  IonSegment,
-  IonSegmentButton,
-} from "@ionic/react";
-import { useHistory } from "react-router-dom";
-import CompletedTasks from "./CompletedTasks";
+import React, { useState, useEffect } from 'react';
+import { IonSegment, IonSegmentButton } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+import CompletedTasks from './CompletedTasks';
 const MyTasks: React.FC = () => {
-  const [selectedSegment, setSelectedSegment] = useState("completed");
+  const [selectedSegment, setSelectedSegment] = useState('completed');
   const handleSegmentChange = (e: CustomEvent) => {
     setSelectedSegment(e.detail.value);
   };
 
   const history = useHistory();
-  
+
   const goBack = () => {
     history.goBack(); // This function navigates back to the previous page
   };
