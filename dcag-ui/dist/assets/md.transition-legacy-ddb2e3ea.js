193fa956db151fcf5c02756d1493ac30a85a5fc0
sameer mishra <smishr81@ext.uber.com>
2024-01-16 08:42:51 +0530

    translation issue fixed; loading issue fixed

--- dcag-ui/dist/assets/md.transition-legacy-ddb2e3ea.js
+++ dcag-ui/dist/assets/md.transition-legacy-ddb2e3ea.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-25d0aeaf.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.g,i=n.b}],execute:function(){
+System.register(["./index-legacy-90eea9b8.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.g,i=n.b}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- dcag-ui/dist/assets/swipe-back-legacy-e4235f5a.js
+++ dcag-ui/dist/assets/swipe-back-legacy-e4235f5a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-25d0aeaf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-90eea9b8.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
