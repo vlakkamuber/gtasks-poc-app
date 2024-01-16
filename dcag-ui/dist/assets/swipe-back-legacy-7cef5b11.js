0367969650aa5f7f76e1f3b27c4a51a46fc290e3
sameer mishra <smishr81@ext.uber.com>
2024-01-16 11:08:50 +0530

    userId saved in local storage

--- dcag-ui/dist/assets/swipe-back-legacy-7cef5b11.js
+++ dcag-ui/dist/assets/swipe-back-legacy-7cef5b11.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-67eab4a5.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-5ff4c61e.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
