ebcc896427f96feec9a5c9948425d0f7151a457c
cprata <cprata@ext.uber.com>
2024-01-17 23:58:17 +0530

    corrects file formate

--- dcag-ui/src/utils/mapTeluguDigitsToNumeric.ts
+++ dcag-ui/src/utils/mapTeluguDigitsToNumeric.ts
@@ -1,4 +1,4 @@
-export function mapTeluguDigitsToNumeric(inputText: string) {
+export function mapTeluguDigitsToNumeric(inputText) {
   const teluguDigitsMap = {
     '౦': '0',
     '౧': '1',
@@ -14,4 +14,15 @@ export function mapTeluguDigitsToNumeric(inputText: string) {
 
   // Replace Telugu digits with corresponding numeric digits
   return inputText.replace(/[౦-౯]/g, (match) => teluguDigitsMap[match]);
+}
+
+export function formatDate(timestamp) {
+  const date = new Date(timestamp);
+
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based, so add 1
+  const year = date.getFullYear();
+
+  const formattedDate = `${day}/${month}/${year}`;
+  return formattedDate;
 }
