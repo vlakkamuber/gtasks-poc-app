45f7fc1ac8015ea4f81c01af1ed9a88879a3a080
sameer mishra <smishr81@ext.uber.com>
2024-01-24 10:14:08 +0530

    latest build added

--- dcag-ui/dist/assets/swipe-back-legacy-5e29fba3.js
+++ dcag-ui/dist/assets/swipe-back-legacy-5e29fba3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-cb1be2fa.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-745b7697.js"],(function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
