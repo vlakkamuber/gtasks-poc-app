617a63e3a3d17f5e574849b26f0ead7d91529ee1
Ankush Kalra <akalra2@ext.uber.com>
2024-01-29 12:21:13 +0530

    added sql generation in text to audio in firebase functions

--- dcag-functions/dcag-record-audio-generation/createAudio.js
+++ dcag-functions/dcag-record-audio-generation/createAudio.js
@@ -1,32 +1,39 @@
 const txtomp3 = require("text-to-mp3");
 const fs = require("fs");
-const names = require('./data')
+const names = require("./data");
 
 txtomp3.attributes = {
   ie: "UTF-8",
-  client : "tw-ob",
+  client: "tw-ob",
   // tl : "It-it"
-  tl : "en-IN"
+  tl: "en-IN",
 };
 
 function textToMp3(name) {
   const fileName = formatName(name);
-  txtomp3.getMp3(name, function(err, binaryStream){
-    if(err){
-      console.log(err);
-      return;
-    }
-    var file = fs.createWriteStream(`${fileName}.mp3`);
-    file.write(binaryStream);
-    file.end();
-  });
+  return txtomp3.saveMP3(name, `${fileName}.mp3`);
 }
 
 function formatName(inputName) {
-  const nameWithUnderscores = inputName.replace(/ /g, '_');
+  const nameWithUnderscores = inputName.replace(/ /g, "_");
   const lowercaseName = nameWithUnderscores.toLowerCase();
 
   return lowercaseName;
 }
 
-names.forEach(name => textToMp3(name));
+
+function createSQLFile() {
+  const sqlfile = "tasks.sql";
+  names.forEach((name) => {
+    fs.writeFile(
+      sqlfile,
+      `INSERT INTO tasks (name, task_type, max_number_of_users, input, status, currency, price, create_time, due_time) VALUES ('${name}', 'AUDIO_TO_AUDIO', 3, '${formatName(
+        name
+      )}.mp3', 'NEW', 'USD', 2.0, '2024-01-12 00:00:00', '2024-01-16 00:00:00');\n`,
+      { flag: "a+" }, err => {}
+    );
+  });
+}
+
+(async () => await Promise.all(names.map((name) => textToMp3(name))))();
+createSQLFile();
