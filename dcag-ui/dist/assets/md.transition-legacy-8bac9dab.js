3cc5dd55f4142a3ef9487d61af47a6b4864c91da
sameer mishra <smishr81@ext.uber.com>
2024-01-29 16:38:15 +0530

    default all category added

--- dcag-ui/dist/assets/md.transition-legacy-8bac9dab.js
+++ dcag-ui/dist/assets/md.transition-legacy-8bac9dab.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-63d78848.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.g,i=n.b}],execute:function(){
+System.register(["./index-legacy-4a8a8e77.js"],(function(n,e){"use strict";var t,i;return{setters:[function(n){t=n.g,i=n.b}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
