4fd4a5b0f758ca13164cc198af9886c3068c01c6
cprata <cprata@ext.uber.com>
2024-02-01 11:20:29 +0530

    corrects training icon

--- dcag-ui/src/pages/Dashboard.tsx
+++ dcag-ui/src/pages/Dashboard.tsx
@@ -1,6 +1,6 @@
 // src/pages/Dashboard.tsx
-import React, {useState,useEffect} from "react";
-import { Redirect, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import {
   IonTabs,
   IonTabBar,
@@ -9,73 +9,74 @@ import {
   IonLabel,
   IonRouterOutlet,
   IonFab,
-  IonFabButton,
-} from "@ionic/react";
-import { home, schoolOutline, person, list,warningSharp } from "ionicons/icons";
+  IonFabButton
+} from '@ionic/react';
+import { home, school, person, list, warningSharp } from 'ionicons/icons';
 import { useTranslation } from 'react-i18next';
-import { useUserAuth } from "../context/UserAuthContext";
-import { useHistory,useLocation } from "react-router-dom";
+import { useUserAuth } from '../context/UserAuthContext';
+import { useHistory, useLocation } from 'react-router-dom';
 
-const Dashboard: React.FC = ({content}) => {
+const Dashboard: React.FC = ({ content }) => {
   const { t } = useTranslation();
   const { user, loading } = useUserAuth();
-  const history = useHistory()
+  const history = useHistory();
   const location = useLocation();
   const [showFab, setShowFab] = useState(true);
   useEffect(() => {
     if (user) {
-      localStorage.setItem("loggedInUser",JSON.stringify(user.uid))
+      localStorage.setItem('loggedInUser', JSON.stringify(user.uid));
     }
-  }, [user])
+  }, [user]);
   useEffect(() => {
-    const isDashboardOrReport = location.pathname.includes("/dashboard/issue");
+    const isDashboardOrReport = location.pathname.includes('/dashboard/issue');
     setShowFab(!isDashboardOrReport);
   }, [location]);
 
-  const goToReportBug = ()=>{
-    history.push("/dashboard/issue");
-  }
+  const goToReportBug = () => {
+    history.push('/dashboard/issue');
+  };
   return (
     <>
-   {showFab && ( <IonFab className="report-issue-fab" onClick={()=>goToReportBug()}>
-        <IonFabButton className="outline-fab-button">
-          <IonIcon icon={warningSharp} />
-        </IonFabButton>
-      </IonFab>)}
-   
-    <IonTabs>
-      <IonRouterOutlet>
-      <Redirect exact from="/dashboard" to="/dashboard/home" />
-        <Route path="/dashboard/home" render={() => <>{content}</>} forceRefresh={true}/>
-        <Route path="/dashboard/training" render={() => <>{content}</>} />
-        <Route path="/dashboard/tasks" render={() => <>{content}</>} forceRefresh={true}/>
-        <Route path="/dashboard/account" render={() => <>{content}</>}/>
-        <Route path="/dashboard/issue" render={() => <>{content}</>}/>
-        <Route path="/dashboard/help" render={() => <>{content}</>}/>
-      </IonRouterOutlet>
+      {showFab && (
+        <IonFab className="report-issue-fab" onClick={() => goToReportBug()}>
+          <IonFabButton className="outline-fab-button">
+            <IonIcon icon={warningSharp} />
+          </IonFabButton>
+        </IonFab>
+      )}
 
-      <IonTabBar slot="bottom">
-        <IonTabButton tab="home" href="/dashboard/home">
-          <IonIcon icon={home} />
-          <IonLabel>{t(`dcag.home.bottomTabs.home`)}</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="tasks" href="/dashboard/tasks">
-          <IonIcon icon={list} />
-          <IonLabel>{t(`dcag.home.bottomTabs.tasks`)}</IonLabel>
-        </IonTabButton>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Redirect exact from="/dashboard" to="/dashboard/home" />
+          <Route path="/dashboard/home" render={() => <>{content}</>} forceRefresh={true} />
+          <Route path="/dashboard/training" render={() => <>{content}</>} />
+          <Route path="/dashboard/tasks" render={() => <>{content}</>} forceRefresh={true} />
+          <Route path="/dashboard/account" render={() => <>{content}</>} />
+          <Route path="/dashboard/issue" render={() => <>{content}</>} />
+          <Route path="/dashboard/help" render={() => <>{content}</>} />
+        </IonRouterOutlet>
 
-        <IonTabButton tab="training" href="/dashboard/training">
-          <IonIcon icon={schoolOutline} />
-          <IonLabel>{t(`dcag.home.bottomTabs.training`)}</IonLabel>
-        </IonTabButton>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="home" href="/dashboard/home">
+            <IonIcon icon={home} />
+            <IonLabel>{t(`dcag.home.bottomTabs.home`)}</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tasks" href="/dashboard/tasks">
+            <IonIcon icon={list} />
+            <IonLabel>{t(`dcag.home.bottomTabs.tasks`)}</IonLabel>
+          </IonTabButton>
 
-        
-        <IonTabButton tab="account" href="/dashboard/account">
-          <IonIcon icon={person} />
-          <IonLabel>{t(`dcag.home.bottomTabs.account`)}</IonLabel>
-        </IonTabButton>
-      </IonTabBar>
-    </IonTabs>
+          <IonTabButton tab="training" href="/dashboard/training">
+            <IonIcon icon={school} />
+            <IonLabel>{t(`dcag.home.bottomTabs.training`)}</IonLabel>
+          </IonTabButton>
+
+          <IonTabButton tab="account" href="/dashboard/account">
+            <IonIcon icon={person} />
+            <IonLabel>{t(`dcag.home.bottomTabs.account`)}</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
     </>
   );
 };
