284ef87498eea867acacf93a12041f60500a68a8
sameer mishra <smishr81@ext.uber.com>
2024-02-03 19:55:06 +0530

    fixed infinite call

--- dcag-ui/src/pages/Training.tsx
+++ dcag-ui/src/pages/Training.tsx
@@ -37,7 +37,7 @@ const Training: React.FC = () => {
   const videoRef = useRef(null);
   useEffect(()=>{
     getTrainingsDoc();
-  },)
+  },[])
 
   const playVideo = () => {
     if (videoRef.current) {
