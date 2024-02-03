845b62a19ad399598771ab0eafe3ee3cd4cd473d
Vinay Lakkam <vlakka1@ext.uber.com>
2024-02-03 14:26:39 +0530

    dcag-task-injestion

---
+++ dcag-functions/dcag-taskingestion/src/test/java/com/uber/dcag/taskingestion/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.dcag.taskingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
