0fc8565c3fc9725d8387ebdcff423c069065c7ab
Sumit <sjinda4@ext.uber.com>
2024-02-06 20:44:05 +0530

    Added param in docs api gateway

--- dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/controller/DcagController.java
+++ dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/controller/DcagController.java
@@ -16,7 +16,7 @@ public class DcagController {
 
     @CrossOrigin
     @GetMapping(value = "/docs", produces = "application/json")
-    ResponseEntity<DocResponse> getDocsUrl(@RequestParam(defaultValue = "training") String type,
+    ResponseEntity<DocResponse> getDocsUrl(@RequestParam(required = false, defaultValue = "training") String type,
                                            @RequestParam(required = false, defaultValue = "english") String language) {
 
        return ResponseEntity.ok(docService.getDocsUrl(type,language));
