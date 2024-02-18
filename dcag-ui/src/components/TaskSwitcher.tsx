3b4e11cc337d563d1d22cae54f1918a9383a81ce
sameer mishra <smishr81@ext.uber.com>
2024-02-18 19:23:49 +0530

    analytics added for switch task

--- dcag-ui/src/components/TaskSwitcher.tsx
+++ dcag-ui/src/components/TaskSwitcher.tsx
@@ -4,6 +4,8 @@ import { useCategory } from '../context/TaskCategoryContext';
 import { Select } from "baseui/select";
 import {SHAPE,SIZE} from 'baseui/button'
 import { useTranslation } from 'react-i18next';
+import useAnalytics from '../hooks/useAnanlytics';
+import { ANALYTICS_PAGE, LANGUAGE_CODE_MAPPER } from '../constants/constant';
 export const TASK_CATEGORIES_DATA = [
   {
     id: 'ALL',
@@ -40,10 +42,12 @@ const taskObj = {
 }
 const TaskSwitcher: React.FC = () => {
   const { selectedCategory, setSelectedCategory} = useCategory();
+  const logEvent = useAnalytics({ page: ANALYTICS_PAGE.tasks });
   const { t } = useTranslation();
   const [value, setValue] = React.useState({label:taskObj.selectedCategory,id:selectedCategory});
   const handleChange = (params) => {
     const category = params[0].id;
+    logEvent({ actions: 'change_task', properties: category });
     localStorage.setItem('selectedCategory', category);
     setSelectedCategory(category);
     setValue({label:taskObj.category,id:category})
