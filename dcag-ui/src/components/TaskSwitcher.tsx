2be47e0a932a70aabae4c1438a2b536b5fe435ff
Ankush Kalra <akalra2@ext.uber.com>
2024-02-21 16:57:27 +0530

    updated TaskSwitcher

--- dcag-ui/src/components/TaskSwitcher.tsx
+++ dcag-ui/src/components/TaskSwitcher.tsx
@@ -1,15 +1,14 @@
-
 import React, { useEffect } from 'react';
 import { useCategory } from '../context/TaskCategoryContext';
-import { Select } from "baseui/select";
-import {SHAPE,SIZE} from 'baseui/button'
 import { useTranslation } from 'react-i18next';
 import useAnalytics from '../hooks/useAnanlytics';
-import { ANALYTICS_PAGE, LANGUAGE_CODE_MAPPER } from '../constants/constant';
+import { ANALYTICS_PAGE } from '../constants/constant';
+import {  IonSelect, IonSelectOption } from '@ionic/react';
+
 export const TASK_CATEGORIES_DATA = [
   {
     id: 'RECORD_AUDIO',
-    label: 'Record Audio',
+    label: 'Record Audio'
   },
   {
     id: 'RECEIPT_DIGITIZATION',
@@ -17,75 +16,68 @@ export const TASK_CATEGORIES_DATA = [
   },
   {
     id: 'LOCALIZATION_QUALITY',
-    label: 'Localization Quality',
+    label: 'Localization Quality'
   },
   {
     id: 'IMAGE_LABELLING',
-    label: 'Image Labelling',
+    label: 'Image Labelling'
   },
   {
     id: 'MENU_PHOTO_REVIEW',
-    label: 'Menu Photo Review',
-  },
+    label: 'Menu Photo Review'
+  }
 ];
 const taskObj = {
-  "MENU_PHOTO_REVIEW":'Menu Photo Review',
-  "IMAGE_LABELLING":"Image Labelling",
-  "LOCALIZATION_QUALITY":'Localization Quality',
-  "RECEIPT_DIGITIZATION":"Receipt Digitization",
-  "RECORD_AUDIO":"Record Audio"
-}
+  MENU_PHOTO_REVIEW: 'Menu Photo Review',
+  IMAGE_LABELLING: 'Image Labelling',
+  LOCALIZATION_QUALITY: 'Localization Quality',
+  RECEIPT_DIGITIZATION: 'Receipt Digitization',
+  RECORD_AUDIO: 'Record Audio'
+};
 const TaskSwitcher: React.FC = () => {
-  const { selectedCategory, setSelectedCategory} = useCategory();
+  const { selectedCategory, setSelectedCategory } = useCategory();
   const logEvent = useAnalytics({ page: ANALYTICS_PAGE.tasks });
   const { t } = useTranslation();
-  const [value, setValue] = React.useState({label:taskObj.selectedCategory,id:selectedCategory});
+  const [value, setValue] = React.useState({
+    label: taskObj.selectedCategory,
+    id: selectedCategory
+  });
   const handleChange = (params) => {
-    const category = params[0].id;
+    const category = params.detail.value;
+    if (!category) {
+      return;
+    }
     logEvent({ actions: 'change_task', properties: category });
     localStorage.setItem('selectedCategory', category);
     setSelectedCategory(category);
-    setValue({label:taskObj.category,id:category})
+    setValue({ label: taskObj.category, id: category });
   };
+
   // Set the default language to English if not found in local storage
   const defaultCategory = localStorage.getItem('selectedCategory') || 'ALL';
+
   useEffect(() => {
     setSelectedCategory(defaultCategory);
-    setValue({label:taskObj.defaultCategory,id:defaultCategory})
+    setValue({ label: taskObj.defaultCategory, id: defaultCategory });
   }, []); // Empty dependency array ensures the effect runs only once on mount
 
   return (
-    <Select
-    options={TASK_CATEGORIES_DATA.map(option => ({
-      ...option,
-      label: t(`dcag.home.taskHub.${option.id}.title`),
-    }))}
-      size={SIZE.compact}
-      shape={SHAPE.pill}
-      clearable={false}
-      placeholder={<span style={{ color: '#000' }}>{t(`dcag.tasks.changeTask.label`)}</span>} 
-      onChange={params => handleChange(params.value)}
-      overrides={{
-        SelectArrow: {
-          style: ({ $theme }) => ({
-            borderRadius: '20px',
-          }),
-        },
-        ControlContainer: {
-          style: ({ $theme }) => ({
-            borderRadius: '20px',
-          }),
-        },
-        DropdownListItem: {
-          style: ({ $theme }) => ({
-            borderTopLeftRadius: '20px', 
-            borderTopRightRadius: '20px', 
-            borderBottomLeftRadius: '20px', 
-            borderBottomRightRadius: '20px', 
-          }),
-        },
-      }}
-    />
+    <IonSelect
+      style={{ background: 'rgb(246, 246, 246)',  paddingLeft: 10, paddingRight: 10, borderRadius: 25 }}
+      compareWith={() => false}
+      placeholder={t(`dcag.tasks.changeTask.label`)}
+      onIonChange={handleChange}>
+      {TASK_CATEGORIES_DATA.map((option) => ({
+        ...option,
+        label: t(`dcag.home.taskHub.${option.id}.title`)
+      })).map((option) => {
+        return (
+          <IonSelectOption key={option.id} value={option.id}>
+            {option.label}
+          </IonSelectOption>
+        );
+      })}
+    </IonSelect>
   );
 };
 
