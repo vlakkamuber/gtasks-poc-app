9759f43ae94c9441f9d84b416b49758e5cda4500
Sumit <sjinda4@ext.uber.com>
2024-03-14 16:52:54 +0530

    Added earning api changes and added bootstap property file to use gcp secret manager in application properties

--- dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/repository/UserTasksRepository.java
+++ dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/repository/UserTasksRepository.java
@@ -8,6 +8,7 @@ import com.ubr.dcagapiservicejava.domain.enums.UserTaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,8 @@ public interface UserTasksRepository extends JpaRepository<UserTask, Long> {
 
     @Query("Select T from UserTask T where T.task.id = :taskId and T.user.userType = 'DRIVER'")
     List<UserTask> findByTaskIdAndUserType(Long taskId);
+
+    @Query("Select T from UserTask T where T.user.id = :userId and T.user.userType = 'DRIVER' and T.status = 'COMPLETED' and T.completionTime >=:startDate and T.completionTime <= :endDate")
+    List<UserTask> getEarnings(String userId, LocalDateTime startDate, LocalDateTime endDate);
 }
 
