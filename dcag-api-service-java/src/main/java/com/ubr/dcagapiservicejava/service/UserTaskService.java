319c30f91bcae2c94c473330fdf14605d90fb992
Sumit <sjinda4@ext.uber.com>
2024-03-26 22:09:16 +0530

    Updated log and limit for expiry task for testing

--- dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/service/UserTaskService.java
+++ dcag-api-service-java/src/main/java/com/ubr/dcagapiservicejava/service/UserTaskService.java
@@ -404,6 +404,7 @@ public class UserTaskService {
         if (!CollectionUtils.isEmpty(userTasks)){
             Set<Long> taskList = new HashSet<>();
             userTasks.forEach(userTask -> {
+                log.info("User task for id: {}",userTask.id());
                 taskList.add(userTask.task().id());
                 userTask.status(UserTaskStatus.EXPIRED);
             });
